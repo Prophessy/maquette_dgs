ef9afe8589f8a6fa52e7737d36efbb50106822c9
Prophessy <cresteyquentin@hotmail.fr>
2022-04-23 13:26:49 +0200

    Avancée sur le responsive

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 $(document).ready(function () {
     $('.slider').slick({
+        autoplay: true,
+        speed: 1000,
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 4,
         prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-arrow-left"></i></button>',
-        nextArrow: '<button class="slide-arrow next-arrow"><i class="fa-solid fa-arrow-right"></i></button>'
+        nextArrow: '<button class="slide-arrow next-arrow"><i class="fa-solid fa-arrow-right"></i></button>',
+        responsive: [
+          {
+            breakpoint: 1400,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+            }
+          },
+          {
+            breakpoint: 1000,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
       });
 });
